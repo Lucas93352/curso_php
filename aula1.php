58d63da8f845e6f31cc66381bde706623411378d
Lucas93352 <lucastomazini2008@gmail.com>
2024-06-18 21:57:42 -0300

    primeiro commit no github

---
+++ aula1.php
@@ -0,0 +1,23 @@
+<?php
+//este é um tipo de comentário
+echo "top<br>";
+echo "se quiser sim<br>";
+/*mais um*/
+#one more 
+
+$texto = "pequeno texto que você está lendo :)<br>";
+echo $texto;
+
+$outravariavel = "one more text<br>"; //olha o texto ai 
+echo $outravariavel;
+
+echo "calculos matemáticos<br>";
+echo 1+1;
+echo "<br>";
+echo 10-7;
+echo "<br>";
+echo 99*10;
+echo "<br>";
+echo 4%2;
+
+?>

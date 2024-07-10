9ce8a2a5b71cd123a8153ce3cf469190de556100
Lucas93352 <lucastomazini2008@gmail.com>
2024-07-09 21:59:52 -0300

    Create aula4.php
    top

---
+++ aula4.php
@@ -0,0 +1,9 @@
+<?php
+// utilizar o laco de repeticao FOR para criar a tabuada do numero informado pelo usuario EX: usuario informou o numero 4, fazer a tabuada do 4, e assim por diante 
+$numero = 9;
+
+for ($i = 0; $i <=9 ; $i++){
+    echo "$numero x $i = " . $numero * $i . "<br>";
+}
+
+?>  

a2a3dd793e253238878c5baec2cbc926f7882007
Lucas93352 <lucastomazini2008@gmail.com>
2024-10-08 21:48:03 -0300

    mvc

---
+++ src/models/UsuarioModel.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once './src/Conexao.php';
+
+class UsuarioModel {
+
+    private $fields = [
+        'id',
+        'cpf',
+        'email',
+        'senha',
+        'excluido',
+        'usuario_alteracao',
+        'data_criacao',
+        'data_atalizacao',
+    ];
+
+    public function create() {
+
+    }
+
+    public function read($id) {
+
+    }
+
+    public function readAll() {
+
+    }
+
+    public function update($id) {
+
+    }
+
+    public function delete($id) {
+
+    }
+    
+}
---
+++ src/models/UsuarioPessoa.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once './src/Conexao.php';
+
+class UsuarioPessoa {
+
+    private $fields = [
+        'id',
+        'nome',
+        'sobrenome',
+        'nome_completo',
+        'email',
+        'cpf',
+        'cnpj',
+        'data_criacao',
+        'data_atalizacao',
+        'sexo',
+        'tipo_pessoa',
+        'estado_civil',
+        'data_aniversario',
+        'id_usuario',
+        'excluido',
+    ];
+
+    public function create() {
+
+    }
+
+    public function read($id) {
+
+    }
+
+    public function readAll() {
+
+    }
+
+    public function update($id) {
+
+    }
+
+    public function delete($id) {
+
+    }
+    
+}

dcb3eef206d33f053ce96f9ae6bdf728a403d447
Lucas93352 <lucastomazini2008@gmail.com>
2024-10-15 21:55:41 -0300

    top

---
+++ src/models/EnderecoModel.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once './src/Conexao.php';
+
+class enderecoModel {
+
+    private $fields = [
+        'id',
+        'rua',
+        'bairro',
+        'numero',
+        'cep',
+        'complemento',
+        'cidade',
+        'estado',
+        'id_pessoa',
+    ];
+
+    public function create() {
+
+    }
+
+    public function read($id) {
+
+    }
+
+    public function readAll() {
+
+    }
+
+    public function update($id) {
+
+    }
+
+    public function delete($id) {
+
+    }
+    
+}
--- src/models/UsuarioModel.php
+++ src/models/UsuarioModel.php
@@ -4,6 +4,7 @@ require_once './src/Conexao.php';
 
 class UsuarioModel {
 
+    private $table = "Usuario";
     private $fields = [
         'id',
         'cpf',
@@ -15,24 +16,55 @@ class UsuarioModel {
         'data_atalizacao',
     ];
 
-    public function create() {
+    public function create($values) {
 
+        $fieldsLocal = $this->fields;
+
+        unset($fieldsLocal['id']);
+        unset($fieldsLocal['usuario_alteracao']);
+        unset($fieldsLocal['data_criacao']);
+        unset($fieldsLocal['data_atalizacao']);
+
+        $fieldsSTR = implode(',',$fieldsLocal);
+        $valuesSTR = implode(',', $values);
+    
+        $sql = ("INSERT INTO ($$this->table) ($fieldsSTR) VALUES ({$valuesSTR});");
     }
 
     public function read($id) {
 
+        $fieldsSTR = implode(',', $this->fields);
+    
+        $sql = ("SELEC ($fieldsSTR) FROM {$this ->table} WHERE id = ($id);");
     }
 
     public function readAll() {
 
+        $fieldsSTR = implode(',', $this->fields);
+    
+        $sql = ("SELEC ($fieldsSTR) FROM {$this ->table}");   // ele esta selecionando todas as colunas da tabla 
     }
 
-    public function update($id) {
+    public function update($id, $values) {
 
+        $fieldsLocal = $this->fields;
+
+        unset($fieldsLocal['id']);
+        unset($fieldsLocal['usuario_alteracao']);
+        unset($fieldsLocal['data_criacao']);
+        unset($fieldsLocal['data_atalizacao']);
+
+        foreach($values as $key => $value) {
+            $fieldsLocal[$key] = $value[$key];
+        }
+
+        $fieldsSTR = implode(',', $fieldsLocal);
+
+        $sql = ("UPDATE {$$this->table} SET $fieldsSTR WHERE ID = {$id};");
     }
 
     public function delete($id) {
-
+        $sql = ("DELET FROM {$this->table} WHERE id ($id);");
     }
     
 }

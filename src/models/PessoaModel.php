700d9f0de9a2efb0717717d6a6324ca69df50e08
Lucas93352 <lucastomazini2008@gmail.com>
2024-10-22 19:06:39 -0300

    Update PessoaModel.php

--- src/models/PessoaModel.php
+++ src/models/PessoaModel.php
@@ -1,45 +1,61 @@
 <?php
 
-require_once './src/Conexao.php';
+require '../database/Conexao.php';
 
-class PessoaModel {
+class PessoaModel extends BaseModel {
+
+    private $table = 'Pessoa';
 
     private $fields = [
-        'id',
         'nome',
         'sobrenome',
         'nome_completo',
         'email',
         'cpf',
         'cnpj',
-        'data_criacao',
-        'data_atalizacao',
         'sexo',
         'tipo_pessoa',
         'estado_civil',
         'data_aniversario',
         'id_usuario',
-        'excluido',
     ];
 
-    public function create() {
+    public function create($values) {
+
+        $this->createAdjust($this->fields, $values);
+
+        $sql = ("INSERT INTO {$this->table} ({$this->fieldsSTR}) VALUES ({$this->valuesSTR});");
+        // INSERT INTO {$this->table} ('cpf','email','senha','excluido') VALUES ('546546654', 'fulanmo@email.com'...)
 
     }
 
     public function read($id) {
+        
+        // 'id','excluido','usuario_alteracao','data_criacao','data_atualizacao','cpf','email','senha';
+        $this->readAdjust($this->fields);
 
+        // SELECT * FROM Usuario WHERE id = 14;
+        $sql = ("SELECT {$this->fieldsSTR} FROM {$this->table} WHERE id = {$id} LIMIT 1;");
     }
 
-    public function readAll() {
+    public function readAll($page = 20) {
+        
+        $this->readAdjust($this->fields);
 
+        $sql = ("SELECT {$this->fieldsSTR} FROM {$this->table} LIMIT {$page};");
     }
 
-    public function update($id) {
+    public function update($id, array $values) {
+
+        $this->updateAdjust($values);
+
+        $sql = ("UPDATE {$this->table} SET $this->fieldsSTR WHERE id = {$id};");
+        // UPDATE USUARIOS SET cpf = '123456', email = 'novoEmail@trallala.com' WHERE id = 1;
 
     }
 
     public function delete($id) {
-
+        $sql = ("DELETE FROM {$this->table} WHERE id = {$id};");
     }
-    
+
 }
